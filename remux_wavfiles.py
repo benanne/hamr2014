146f41b1b266855d14d7d79167fa57f1b55f51f4
Sander Dieleman <sanderdieleman@gmail.com>
2014-10-26 15:59:12 +0800

    normalizing with sox doesn't work, try remuxing with avconv instead

---
+++ remux_wavfiles.py
@@ -0,0 +1,38 @@
+import sys
+import os
+import glob
+import random
+
+import h5py
+
+
+SOURCE_PATH = "/home/sedielem/data/urbansound8k/UrbanSound8K/audio"
+TARGET_PATH = "/home/sedielem/data/urbansound8k/audio_remuxed"
+
+SIZE = 8732
+N_FOLDS = 10
+
+
+i = 0
+for f in xrange(1, N_FOLDS + 1):
+    print "fold %d" % f
+    fold_path = os.path.join(SOURCE_PATH, "fold%d" % f)
+    fold_target_path = os.path.join(TARGET_PATH, "fold%d" % f)
+
+    if not os.path.exists(fold_target_path):
+        os.makedirs(fold_target_path)
+
+    clip_paths = glob.glob(os.path.join(fold_path, "*.wav"))
+
+    for clip_path in clip_paths:
+        print "  clip %d of %d" % (i + 1, SIZE)
+        print "  %s" % clip_path
+
+        clip_target_path = os.path.join(fold_target_path, os.path.basename(clip_path))
+        command = "avconv -i %s %s" % (clip_path, clip_target_path)
+        print command
+        os.system(command)
+
+        i += 1
+
+print "done"
