73a6ebac85de74dc3d3da55a81de6b33e4b0e0e7
Sander Dieleman <sanderdieleman@gmail.com>
2014-10-26 15:37:32 +0800

    sox to the rescue

---
+++ normalize_wavfiles.py
@@ -0,0 +1,34 @@
+import sys
+import os
+import glob
+import random
+
+import h5py
+
+
+SOURCE_PATH = "/home/sedielem/data/urbansound8k/UrbanSound8K/audio"
+TARGET_PATH = "home/sedielem/data/urbansound8k/audio_normalized"
+N_FOLDS = 10
+SAMPLERATE = 22050
+
+
+for f in xrange(1, N_FOLDS + 1):
+    print "fold %d" % f
+    fold_path = os.path.join(SOURCE_PATH, "fold%d" % f)
+    fold_target_path = os.path.join(TARGET_PATH, "fold%d" % f)
+
+    if not os.path.exists(fold_target_path):
+        os.makedirs(fold_target_path)
+
+    clip_paths = glob.glob(os.path.join(fold_path, "*.wav"))
+
+    for clip_path in clip_paths:
+        print "  clip %d of %d" % (i + 1, SIZE)
+        print "  %s" % clip_path
+
+        clip_target_path = os.path.join(fold_target_path, os.path.basename(clip_path))
+        command = "sox %s -r %d -c 1 %s" % (clip_path, SAMPLERATE, clip_target_path)
+        print command
+        os.system(command)
+
+print "done"
